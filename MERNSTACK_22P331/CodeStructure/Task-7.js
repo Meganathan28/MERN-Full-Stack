9832c153cedb01337d0ccdcc6240372cd1e183d4
Meganathan Saravanan <153720225+Meganathan28@users.noreply.github.com>
2024-08-15 12:36:00 +0530

    Update Task-7.js

--- MERNSTACK_22P331/CodeStructure/Task-7.js
+++ MERNSTACK_22P331/CodeStructure/Task-7.js
@@ -2,6 +2,6 @@ let name="Kirthi"; let age=21;
 // let name1="Black widow" let age1=22; 
 
 // When I use semicolon in the line.It didn't show any error to me.
-//whereas I don't use semicolon in the line .It shoes me SyntaxError: unexpected token: 'let';
+//whereas I don't use semicolon in the line .It shows me SyntaxError: unexpected token: 'let';
 // Semicolon isn't mandatory if we use "enter" for next line and the variables got seperated without nay error.
 //If you work on a single line Semicolon(;) is must for seperation.
